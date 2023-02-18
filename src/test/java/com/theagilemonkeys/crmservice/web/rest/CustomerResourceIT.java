525f0e4061c4668d9ef212d2b8ad82cac5daa460
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2023-02-18 16:24:39 +0100

    Added test for returning a customer list

--- src/test/java/com/theagilemonkeys/crmservice/web/rest/CustomerResourceIT.java
+++ src/test/java/com/theagilemonkeys/crmservice/web/rest/CustomerResourceIT.java
@@ -1,6 +1,9 @@
 package com.theagilemonkeys.crmservice.web.rest;
 
 import com.theagilemonkeys.crmservice.IntegrationTest;
+import com.theagilemonkeys.crmservice.domain.Customer;
+import com.theagilemonkeys.crmservice.repository.CustomerRepository;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -15,13 +18,37 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 class CustomerResourceIT {
 
+    private static final String DEFAULT_NAME = "AAAAAAAAAA";
+    private static final String UPDATED_NAME = "BBBBBBBBBB";
+
+    private static final String DEFAULT_SURNAME = "AAAAAAAAAA";
+    private static final String UPDATED_SURNAME = "BBBBBBBBBB";
+
     @Autowired
-    private MockMvc restConsumerMockMvc;
+    private CustomerRepository customerRepository;
+
+    @Autowired
+    private MockMvc restCustomerMockMvc;
+
+    private Customer customer;
+
+    @BeforeEach
+    void setUp() {
+        customerRepository.deleteAll();
+        customer = createDefaultCustomer();
+    }
+
+    private Customer createDefaultCustomer() {
+        Customer customer = new Customer();
+        customer.setName(DEFAULT_NAME);
+        customer.setSurname(DEFAULT_SURNAME);
+        return customerRepository.save(customer);
+    }
 
     @Test
     @WithMockUser
-    void should_return_empty_consumer_list() throws Exception {
-        restConsumerMockMvc.perform(get("/customers")
+    void should_return_empty_customer_list() throws Exception {
+        restCustomerMockMvc.perform(get("/customers")
                         .contentType(APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$").isArray())
@@ -31,8 +58,8 @@ class CustomerResourceIT {
 
     @Test
     @WithMockUser(roles = "ADMIN")
-    void should_return_empty_consumer_list_as_admin() throws Exception {
-        restConsumerMockMvc.perform(get("/customers")
+    void should_return_empty_customer_list_as_admin() throws Exception {
+        restCustomerMockMvc.perform(get("/customers")
                         .contentType(APPLICATION_JSON))
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$").isArray())
@@ -41,10 +68,23 @@ class CustomerResourceIT {
     }
 
     @Test
-    void should_not_allow_to_access_consumer_list_as_anonymous() throws Exception {
-        restConsumerMockMvc.perform(get("/customers")
+    void should_not_allow_to_access_customer_list_as_anonymous() throws Exception {
+        restCustomerMockMvc.perform(get("/customers")
                         .contentType(APPLICATION_JSON))
                 .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser
+    void should_return_customers() throws Exception {
+        restCustomerMockMvc.perform(get("/customers")
+                        .contentType(APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$").isArray())
+                .andExpect(jsonPath("$.[0].id").value(customer.id()))
+                .andExpect(jsonPath("$.[0].name").value(customer.name()))
+                .andExpect(jsonPath("$.[0].surname").value(customer.surname()))
+                .andExpect(content().contentType(APPLICATION_JSON));
     }
 
 }
