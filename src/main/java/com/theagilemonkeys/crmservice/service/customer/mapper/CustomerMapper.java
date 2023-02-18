78c577df51249b35a5154de3c5dc41f263ca0715
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2023-02-18 16:26:10 +0100

    Added id field to CustomerDTO

--- src/main/java/com/theagilemonkeys/crmservice/service/customer/mapper/CustomerMapper.java
+++ src/main/java/com/theagilemonkeys/crmservice/service/customer/mapper/CustomerMapper.java
@@ -4,10 +4,13 @@ import com.theagilemonkeys.crmservice.domain.Customer;
 import com.theagilemonkeys.crmservice.service.customer.dto.CustomerDTO;
 import org.springframework.stereotype.Component;
 
+import static java.lang.String.valueOf;
+
 @Component
 public class CustomerMapper {
     public CustomerDTO toDTO(Customer customer) {
         return CustomerDTO.builder()
+                .id(valueOf(customer.id()))
                 .name(customer.name())
                 .surname(customer.surname())
                 .photoUrl(customer.photoUrl())
