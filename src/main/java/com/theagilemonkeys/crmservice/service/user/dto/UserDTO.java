c11afe768e69aaf79a1c5896adba9674ba5e83ae
Arnau Garcia <arnau.garcia.gallego@gmail.com>
2023-02-22 18:19:01 +0100

    Added ID to UserDTO

--- src/main/java/com/theagilemonkeys/crmservice/service/user/dto/UserDTO.java
+++ src/main/java/com/theagilemonkeys/crmservice/service/user/dto/UserDTO.java
@@ -5,5 +5,5 @@ import lombok.Builder;
 import java.io.Serializable;
 
 @Builder
-public record UserDTO(String name, String surname, String email, String imageUrl, String[] roles) implements Serializable {
+public record UserDTO(String id, String name, String surname, String email, String imageUrl, String[] roles) implements Serializable {
 }
--- src/main/java/com/theagilemonkeys/crmservice/service/user/mapper/UserMapper.java
+++ src/main/java/com/theagilemonkeys/crmservice/service/user/mapper/UserMapper.java
@@ -4,10 +4,13 @@ import com.theagilemonkeys.crmservice.domain.User;
 import com.theagilemonkeys.crmservice.service.user.dto.UserDTO;
 import org.springframework.stereotype.Component;
 
+import static java.lang.String.valueOf;
+
 @Component
 public class UserMapper {
     public UserDTO toDTO(User user) {
         return UserDTO.builder()
+                .id(valueOf(user.id()))
                 .name(user.name())
                 .surname(user.surname())
                 .email(user.email())
